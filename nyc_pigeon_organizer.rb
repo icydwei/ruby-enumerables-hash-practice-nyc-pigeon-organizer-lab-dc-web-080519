f33e3f4810f1a0cf12499d3ecef22cfe641daa59
Rob Paik <robpaik@gmail.com>
2019-07-12 20:44:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 pigeon_list = {}  
   data[:color].reduce({}) do |memo, (colors, names)|
-    binding.pry
     names.each do |name|
+    binding.pry
     if pigeon_list.include?(name)
       binding.pry
       pigeon_list[name][:color] = "purple"
