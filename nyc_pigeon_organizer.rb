1c9a085781d466de3d700f3cbc91630c20b8be42
Rob Paik <robpaik@gmail.com>
2019-07-12 20:33:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
 pigeon_list = {}  
   data[:color].each do |colors|
     binding.pry
-    colors.each do |names|
+    colors.each do |color, names|
       binding.pry
       if pigeon_list.include?(names)
         pigeon_list[names][:color] 
